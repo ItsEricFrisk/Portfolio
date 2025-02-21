9ebb9a5f4adaebfe0a4cd71da1b5f02b2e654ba4
Eric Frisk <125310875+ItsEricFrisk@users.noreply.github.com>
2025-02-21 08:47:43 +0100

    removed some p tags

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -62,26 +62,6 @@ const About = () => {
             >
                Hi, I’m a passionate frontend developer with a strong interest in creating user-friendly and visually appealing web applications. I’m currently completing my studies in frontend development at Folkuniversitetet, graduating in June.
             </motion.p>
-
-            <motion.p       
-                initial={textAnimationP.initial}
-                whileInView={textAnimationP.whileInView}
-                transition={textAnimationP.transition} 
-                viewport={textAnimationP.viewport}
-                className="text-xl md:text-5xl my-10 md:my-20"
-            >
-                I live in Östersund with my partner, who is also studying here. Originally, I’m from Köping, where I was born and raised. I moved to Östersund about three years ago and have enjoyed exploring the area and its rich nature.
-            </motion.p>
-
-            <motion.p       
-                initial={textAnimationP.initial}
-                whileInView={textAnimationP.whileInView}
-                transition={textAnimationP.transition} 
-                viewport={textAnimationP.viewport}
-                className="text-xl md:text-5xl my-10 md:my-20"
-            >
-                In my free time, I usually work on coding projects or explore new technologies. When I’m not sitting at my computer, I’m probably exploring the city, trying new restaurants, or taking long walks with my partner. 
-            </motion.p>
         </section>
     );
 };
